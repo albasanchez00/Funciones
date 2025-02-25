a324cfac5eb1275a1992da98b01d2cc6aefbef48
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-25 20:31:29 +0100

    Actividad Propuesta 6 y 7 Funciones

--- src/Actividades/Actividad_Propuesta_5.java
+++ src/Actividades/Actividad_Propuesta_5.java
@@ -1,5 +1,5 @@
 package Actividades;
-
+import java.util.Scanner;
 
 /* Actividad. Mostrar los siguientes datos al uasuario
 Al final mostrarlos de forma organizada en pantalla:
@@ -13,32 +13,73 @@ Al final mostrarlos de forma organizada en pantalla:
 * Mediante los metodos → setter && getter -> POO
 */
 
-import java.util.Scanner;
-
 public class Actividad_Propuesta_5 {
     public static void main(String[] args) {
         mostrarDatos();
     }
 
 
+    /**
+     * Función mediante la que definiremos la variable y mostraremos los datos ingresados.
+    */
     static void mostrarDatos(){
         String nombre=leerDato("nombre");
         String apellidos=leerDato("apellidos");
-        String telefono=leerDato("telefono");
+        int telefono=leerDato();
         String direccion=leerDato("direccion");
         String email=leerDato("email");
-        System.out.println("Nombre: "+nombre+" | Apellido: "+apellidos+" | Telefono: "+telefono+" | Dirección: "+direccion+" | Email: "+email);
+        byte edad=leerNumero("edad");
+        int dni=leerNumero(); // Irá a la función que no espera ningún pase de parámetros.
+        System.out.println("Nombre: "+nombre+" | Apellido: "+apellidos+" | Telefono: "+telefono+
+          " | Dirección: "+direccion+" | Email: "+email+" | DNI: "+dni);
     }
+
 
     /**
      * Función genérica donde se le pasa el valor que quiere leer y devuelve lo qye el usuario a introducido.
      * @param dato
      * @return
-     */
+    */
     static String leerDato(String dato){
         Scanner sc=new Scanner(System.in);
         System.out.print("Indique su "+dato+" → ");
         return sc.nextLine();
+    }
+
+
+    /**
+     * Función genérica donde se le pasa el valor que quiere leer y devuelve un número.
+     * @param dato
+     * @return
+    */
+    static byte leerNumero(String dato){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Indique su "+dato+" → ");
+        return sc.nextByte(); // → Devuelve un número.
+    }
+
+
+    /**
+     * Sobre carga de funciones...
+     * Se puede dar el caso de llamar igual dos funciones.
+     * Pero según el paso de parámertos, irá al que corresponda.
+     * @return
+    */
+    static int leerNumero(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Indique su número de DNI (sin letra) → ");
+        return sc.nextInt(); // → Devuelve un número entero.
+    }
+
+
+    /**
+     * Otro ejemplo → Sobre carga de funciones, ejemplo con la función leerDato()
+     * @return
+     */
+    static int leerDato(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Indique su número de teléfono → ");
+        return sc.nextInt();
     }
 
 
---
+++ src/Actividades/Actividad_Propuesta_7.java
@@ -0,0 +1,13 @@
+package Actividades;
+
+/* Actividades.
+1. Implementa una funcion convertirMoneda(), que reciba una cantidad en dolares Canadienses o Americanos y la convierta a euros
+2. Implementa una función convertirGradosCelsius() que reciba en ºF y los convierta a ºC
+3. Implementa una función pasarMilimetros(), donde reciba el metro o centrimetro y lo pase a milimetros
+4. Sabemos que las personas americanas se pesan en libras. Indica el peso en libras y pasalo a KG.
+5. Indica el tiempo que falta para llegar a tu destino, teniendo en cuenta el tipo de vía transitada (autovía/carretraNacional) y los KM desde el origen hasta el destino.
+6. Indica los segundos y conviertelos en días, horas, minutos y segundos
+*/
+
+public class Actividad_Propuesta_7 {
+}
